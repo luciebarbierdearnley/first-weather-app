74bd76949c9a4885119cfda06aaab9a9c1c446bf
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-01-19 16:34:46 +0000

    Added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("Helloe");
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input");
+  let heading = document.querySelector("h1");
+
+  heading.innerHTML = city.value;
+}
+
+let search = document.querySelector("#search-form");
+search.addEventListener("submit", showCity);
